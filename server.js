0b1f5296acaf0c9952eafd041c2b59db6b9a5ae7
Mahak Agarwal <mahak@insuredmine.com>
2026-02-21 23:13:01 +0530

    minor change

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors())
-app.use('/', router);
+app.use('/api', router);
 app.use(morgan('combined'))
 
 // Basic route
